ea337ca0de4e4244c106bbb3ced8aaaae68cfc11
Etienne Benson <esbenson@gmail.com>
2011-10-12 15:47:08 +0200

    minor fix to urls

--- pbear/fedregfeed/urls.py
+++ pbear/fedregfeed/urls.py
@@ -4,5 +4,7 @@ from utils import LatestPolarBearUpdate
 urlpatterns = patterns('fedregfeed',
     url(r'^(?P<display_offset>\d+)/(?P<display_num>\d+)/$', 'views.index'), # calls the update/display view with custom arguments 
     url(r'^$', 'views.index', {'display_offset':0, 'display_num':20}), # calls the update/display view with default arguments (i.e., show the beginning of the list)
+    url(r'^fedregrfeed/$', 'views.index', {'display_offset':0, 'display_num':20}), # calls the update/display view with default arguments (i.e., show the beginning of the list)
     (r'^feed/$', LatestPolarBearUpdate()) # syndication
+    (r'^fedregfeed/feed/$', LatestPolarBearUpdate()) # syndication
  )
