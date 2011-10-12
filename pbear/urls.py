578a3e6d8bda0612a55bd9fb7a3732f42fbc460f
Etienne Benson <esbenson@gmail.com>
2011-10-12 15:42:23 +0200

    changed root directory to / and turned off debug

--- pbear/urls.py
+++ pbear/urls.py
@@ -7,7 +7,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mmpafeed.views.home', name='home'),
-    (r'^fedregfeed/', include('fedregfeed.urls')),
+    (r'', include('fedregfeed.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
