bfadff4d7b0fb63e162cdda2627514cc30582e83
Etienne Benson <esbenson@gmail.com>
2011-10-20 11:21:45 +0200

    changed comments function and multiple/single views

--- pbear/fedregfeed/urls.py
+++ pbear/fedregfeed/urls.py
@@ -3,10 +3,11 @@ from utils import LatestPolarBearUpdate
 
 urlpatterns = patterns('fedregfeed.views',
     url(r'^list/(?P<doc_pk>\d+)/$', 'multiple', {'display_num':20}, name='pbear_list'), # calls the update/display view with custom arguments - initial record and num to display
-    url(r'^list/$', 'multiple', {'display_num':20}, name='pbear_list_default'), # calls the update/display view with custom arguments - initial record and num to display
     url(r'^single/(?P<doc_pk>\d+)/comment_posted/$', 'single', {'comment':True}, name='pbear_single_comment_posted'), # calls view for single record  after comment posted
     url(r'^single/(?P<doc_pk>\d+)/$', 'single', name='pbear_single_pk'), # calls the detail view for single record 
-    url(r'^$', 'single', name='pbear_single_default'), 
+    url(r'^list/$', 'multiple', {'display_num':20}, name='pbear_list_default'), # calls the update/display view with custom arguments - initial record and num to display
+    url(r'^$', 'multiple', {'display_num':20}, name='pbear_list_default'), # calls the update/display view with default arguments - (no pk) and num to display
+    #url(r'^$', 'single', name='pbear_single_default'), 
 )
 
 urlpatterns += patterns('fedregfeed',
