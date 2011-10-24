a4a09572e9f526462b3708e3889077cf9b352416
Etienne Benson <esbenson@gmail.com>
2011-10-24 23:38:15 +0200

    refactor in utils, fixed nav

--- pbear/fedregfeed/models.py
+++ pbear/fedregfeed/models.py
@@ -26,12 +26,6 @@ class FedRegDoc(models.Model):
     def __unicode__(self):
         return self.title
     
-class SearchResult(models.Model):
-    results=models.TextField(null=True, blank=True)
-    count=models.IntegerField(null=True, blank=True)
-    total_pages=models.IntegerField(null=True, blank=True)
-    next_page_url=models.CharField(max_length=200, null=True, blank=True)
-       
 class FedRegDocModerator(CommentModerator):
     email_notification = False
  #   auto_close_field   = 'posted_date'
