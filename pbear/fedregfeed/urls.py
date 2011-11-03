d3b1465b802bae558de957e54e99d4352ebe0084
Etienne Benson <esbenson@gmail.com>
2011-11-02 22:57:09 -0400

    refactored multiple view, prelim work on search functionality

--- pbear/fedregfeed/urls.py
+++ pbear/fedregfeed/urls.py
@@ -5,12 +5,12 @@ display_num=10
 
 urlpatterns = patterns('fedregfeed.views',
     url(r'^list/(?P<doc_pk>\d+)/$', 'multiple', {'display_num':display_num}, name='pbear_list'), # calls the update/display view with custom arguments - initial record and num to display
-    url(r'^list/$', 'multiple', {'display_num':display_num}, name='pbear_list_default'), # calls the update/display view with custom arguments - initial record and num to display
     url(r'^single/(?P<doc_pk>\d+)/comment_posted/$', 'single', {'comment':True}, name='pbear_single_comment_posted'), # calls view for single record  after comment posted
     url(r'^single/(?P<doc_pk>\d+)/$', 'single', name='pbear_single_pk'), # calls the detail view for single record 
     url(r'^chart/$', 'pbear_chart', {'search_term':'\"polar bear\"|\"polar bears\"', 'end_year':2011, 'start_year':1994}, name='pbear_chart'),
-    url(r'^$', 'multiple', {'display_num':display_num}, name='pbear_list_default'), # calls the update/display view with default arguments - (no pk) and num to display
-    url(r'^add_html_full_text/$', 'add_html_full_text_to_all'),
+    url(r'^list/$', 'multiple', {'display_num':display_num, 'update_database_flag':True}, name='pbear_list_default'), # calls the update/display view with default args
+    url(r'^$', 'multiple', {'display_num':display_num, 'update_database_flag':True}, name='pbear_list_default'), # calls the update/display view with default arguments - (no pk) and num to display and update_database_flag=True
+    #url(r'^add_html_full_text/$', 'add_html_full_text_to_all'), # for one-time run
     url(r'^trophy/$', 'show_trophy', name='pbear_trophy_map'),
 )
 
