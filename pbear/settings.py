cf140902bf0eb5a802a2b25005f1cb78f7b8c314
Etienne Benson <esbenson@gmail.com>
2011-12-27 22:17:45 -0600

    static files

--- pbear/settings.py
+++ pbear/settings.py
@@ -58,7 +58,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/app/pbear/fedregfeed/static/'
+STATIC_ROOT =  '' #'/home/etienne/pbearfeed/pbear/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -67,11 +67,11 @@ STATIC_URL = '/static/'
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
 # Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = ''
+ADMIN_MEDIA_PREFIX = '/static/admin/media/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    '/home/etienne/pbearfeed/lib/python2.7/site-packages/django/contrib/admin/media/',
+    #    '/home/etienne/pbearfeed/lib/python2.7/site-packages/django/contrib/admin/media/',
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
