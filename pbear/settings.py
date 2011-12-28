59298a569e33088f94a3de00458c411b9e970ac9
Etienne Benson <esbenson@gmail.com>
2011-12-27 23:21:05 -0600

    markup

--- pbear/settings.py
+++ pbear/settings.py
@@ -58,7 +58,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '' #'/home/etienne/pbearfeed/pbear/static/'
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -118,6 +118,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.markup',
 #   my apps
     'fedregfeed',
 #    'fedregfeedcomments', # see COMMENTS_APP parameter below
