e362568552f0e29b6c8389ca6cc688c2152fb676
Etienne Benson <esbenson@gmail.com>
2012-01-08 16:23:55 +0100

    consolidated unused code in unused.py and templates in unused.html

--- pbear/fedregfeed/unused.py
+++ pbear/fedregfeed/unused.py
@@ -104,3 +104,40 @@ def list_view(request, **kwargs):
 
 
 
+
+# NAV BLOCK REMOVED FROM DETAILS 
+    # find primary key for nav to newer/newest record
+    newest_pk = FedRegDoc.objects.order_by('-publication_date')[0].pk
+    if newest_pk == doc.pk:
+        newest_pk = None
+        newer_pk = None
+    else:
+        q = list(FedRegDoc.objects.order_by('publication_date').filter(publication_date__gte = doc.publication_date).distinct())
+        i = 0
+        for i in range(len(q)):
+            if q[i].pk == doc.pk:
+                break
+        try:
+            newer_pk = q[i+1].pk
+        except IndexError:
+            newer_pk = None
+            newest_pk = None
+
+    # find primary key for nav to older/oldest record
+    oldest_pk = FedRegDoc.objects.order_by('publication_date')[0].pk
+    if oldest_pk == doc.pk:
+        oldest_pk = None
+        older_pk = None
+    else:
+        q = list(FedRegDoc.objects.order_by('-publication_date').filter(publication_date__lte=doc.publication_date).distinct())
+        i = 0
+        for i in range(len(q)):
+            if q[i].pk == doc.pk:
+                break
+        try:
+            older_pk = q[i+1].pk
+        except IndexError:
+            older_pk = None
+            oldest_pk = None
+
+
